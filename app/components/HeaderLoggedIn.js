0035acb58ac953739c27857b8a871511de9220b7
luAr26 <raul_cosmin_26@yahoo.com>
2022-01-30 12:59:23 +0200

    Add create post screen

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-function HeaderLoogedIn(props) {
+function HeaderLoggedIn(props) {
   function handleLogout() {
     props.setLoggedIn(false);
     localStorage.removeItem("complexAppToken");
@@ -22,9 +23,9 @@ function HeaderLoogedIn(props) {
           src={localStorage.getItem("complexAppAvatar")}
         />
       </a>
-      <a className="btn btn-sm btn-success mr-2" href="/create-post">
+      <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
-      </a>
+      </Link>
       <button onClick={handleLogout} className="btn btn-sm btn-secondary">
         Sign Out
       </button>
@@ -32,4 +33,4 @@ function HeaderLoogedIn(props) {
   );
 }
 
-export default HeaderLoogedIn;
+export default HeaderLoggedIn;
