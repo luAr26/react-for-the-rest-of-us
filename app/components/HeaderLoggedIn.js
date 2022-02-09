f3c304cab98788604fc5b42a0410c09cc98ae887
luAr26 <raul_cosmin_26@yahoo.com>
2022-02-09 18:46:43 +0200

    Add follow user feature

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import ReactTooltip from "react-tooltip";
 import DispatchContext from "../DispatchContext";
